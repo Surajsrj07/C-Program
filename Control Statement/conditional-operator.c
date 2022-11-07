e40936fc3eb2cd0aa93d9e6c367a266f77dd5406
Surajsrj07 <Surajsrj93@gmail.com>
2022-11-07 23:18:12 +0530

    conditional operator

---
+++ Control Statement/conditional-operator.c
@@ -0,0 +1,17 @@
+//conditional statement ?:
+
+
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c;
+    printf("\n Enter the value of A and B : \n");
+    scanf("%d%d",&a,&b);
+
+    c=a>b?a:b;
+    printf("\nGreatest Number is : %d",c);
+
+    return 0;
+}
+
