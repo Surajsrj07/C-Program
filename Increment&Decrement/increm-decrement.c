97146e48072ff8bb1d935ed297127f804f874b08
Surajsrj07 <Surajsrj93@gmail.com>
2022-11-07 16:44:52 +0530

    increment and decrement

---
+++ Increment&Decrement/increm-decrement.c
@@ -0,0 +1,18 @@
+
+
+#include<stdio.h>
+
+int main()
+{
+    int a=5;
+
+    printf("\n Pre-Increment  :%d ",++a); //6
+    printf("\n Post-Increment :%d ",a++); //6
+    printf("\n A :%d ",a); //7
+
+     printf("\n Pre-Decrement  :%d ",--a); //6
+    printf("\n Post-Decrement :%d ",a--);  //6
+    printf("\n A :%d ",a); //5
+   
+
+}
