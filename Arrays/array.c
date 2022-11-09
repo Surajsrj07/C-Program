a78c2abf27693df028a4c652e6b8c58e13bd9d15
Surajsrj07 <Surajsrj93@gmail.com>
2022-11-09 22:35:27 +0530

    one dimension array

---
+++ Arrays/array.c
@@ -0,0 +1,23 @@
+
+
+#include<stdio.h>
+
+int main()
+{
+    int i,a[100],n;
+    printf("\nEnter the limits : ");
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+    {
+        printf("\nEnter the Number : ");
+        scanf("%d",&a[i]);
+    }
+
+     for(i=0;i<n;i++)
+    {
+        printf("\n%d",a[i]);
+    }
+
+    return 0;
+}
