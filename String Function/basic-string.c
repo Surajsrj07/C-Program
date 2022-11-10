2f8d47647490ade3afa5625af826071ad9933c32
Surajsrj07 <Surajsrj93@gmail.com>
2022-11-10 14:53:26 +0530

    string

---
+++ String Function/basic-string.c
@@ -0,0 +1,17 @@
+//String Function
+
+
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char c[20];
+    printf("\nEnter the String : "); //Ram kumar
+    //scanf("%s",c);     //for string we use %s and can use ampersand symbol
+    gets(c);   
+    
+    printf("\n %s",c);  //Ram kumar
+
+    return 0;
+}
---
+++ String Function/inbuild-fns.c
@@ -0,0 +1,25 @@
+
+
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char c[20],a[20];
+    char x[10]={'r','a','m','\0'};
+    char y[10]={'k','u','m','a','r'};
+    printf("\nEnter the string : ");
+    gets(c);
+
+    printf("\n%s",c);
+    printf("\nLength    : %d",strlen(c));  //String Length
+    printf("\nReverse   : %s",strrev(c));  //String Reverse
+    printf("\nUppercase :%s",strupr(c)); //String UPPERCASE
+    printf("\nLowercase :%s",strlwr(c)); //String Lowercase
+    printf("\nCompare   :%d",strcmp(x,y)); //String Compare
+    printf("\nCopy      :%s",strcpy(a,c)); //String Copy
+    strcat(x,y);
+    printf("\nConacatenation :%s",x);  //STRING Concatenation
+  
+    return 0;
+}
