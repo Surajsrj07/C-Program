97cde699def762a68f8048ae0daa92beedaa5ecc
Surajsrj07 <Surajsrj93@gmail.com>
2022-11-10 22:51:30 +0530

    recursion

---
+++ Functions in c/RECURSION FN/RECURSION.C
@@ -0,0 +1,22 @@
+//Recursion fn
+//factorial of the number
+
+#include<stdio.h>
+
+int factorial(int i) //5
+{
+    if(i<=1)
+    {
+        return 1;
+    }
+  
+    return i*factorial(i-1);  //5*4*3*2*1
+
+}
+
+int main()
+{
+    printf("Factorial : %d",factorial(5)); //120
+    return 0;
+}
+
