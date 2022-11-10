5fe99b98a5968efd181d79d6cbbd81341387bfc8
Surajsrj07 <Surajsrj93@gmail.com>
2022-11-10 23:52:55 +0530

    enum

---
+++ ENUM/enumeration.c
@@ -0,0 +1,17 @@
+//Enumeration or enum variable
+
+#include<stdio.h>
+
+enum bool{No=10,Yes=15};   // 0 1
+int main()
+{
+enum point{x=2,y};  //acts as  2 + 1=> output =3
+enum bool a;
+a=Yes;
+enum bool b;
+b=y;
+printf("\n a : %d",a);
+printf("\n b : %d",b);
+
+return 0;
+}
