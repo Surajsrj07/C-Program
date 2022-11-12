7a8735a21c8dded0a441d28f6094526dceecce7e
Surajsrj07 <Surajsrj93@gmail.com>
2022-11-12 22:06:57 +0530

    case 2

--- Pointers/Using const in Pointer/cases.c
+++ Pointers/Using const in Pointer/cases.c
@@ -5,14 +5,30 @@
 
 int main()
 {
-    char a[3]={'a','b','c'};
+ 
+}
+
+
+/*case 1:
+  char a[3]={'a','b','c'};
     const char *p=a;
 
-    printf("\n *p : %c",*p);
+    printf("\n *p = %c",*p);
     //*p=x;
     p++;
-    printf("\n *p : %c",*p);
+    printf("\n *p = %c",*p);*/
+
+
+/*case 2:
+ char a[3]={'a','b','c'};
+  char const *p=a;
+
+  printf("\n *p = %c",*p);
+  //*p=x;
+  p++;
+  printf("\n *p = %c",*p);
+
 
     return 0;
 
-}
+*/
