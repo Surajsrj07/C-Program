4459571c496e7c0690faaad6278b330b872a8071
Surajsrj07 <Surajsrj93@gmail.com>
2022-11-12 23:29:22 +0530

    initialization and accessing

---
+++ Structure in C/initial-and-accessing.c
@@ -0,0 +1,29 @@
+//Initialization and accessing the member structure in c
+
+
+//Structure in C
+
+#include<stdio.h>
+
+
+struct student
+{
+
+    char *name;
+    int age;
+    float per;
+};
+
+int main()
+
+{
+    struct student o={"Suraj Yeshvanth",28,85.88};
+   
+ 
+
+  printf("\nName           :%s",o.name);
+  printf("\nAge            :%d",o.age);
+  printf("\nPercentage     :%0.2f",o.per);
+
+return 0;
+}
