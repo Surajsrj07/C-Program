185322e8941f9a6551020dd1104723f556da55ac
Surajsrj07 <Surajsrj93@gmail.com>
2022-11-12 21:32:42 +0530

    dangling ptr

---
+++ Pointers/Dangling Ptr/basic.c
@@ -0,0 +1,19 @@
+//Dangling pointer
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int * value()
+{
+    int a=10;
+    return &a;
+}
+
+int main()
+{
+    int *ptr=NULL;
+    ptr=value();
+    printf("%d",*ptr);
+    return 0;
+}
